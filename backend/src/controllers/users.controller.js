12a321c4fc4a7526b4c5836b4d27ec6c359c2a67
Bryan Quiroz <bryan.quiroz@uabc.edu.mx>
2019-10-23 18:30:02 -0700

    MERN Stack Video 7

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -1,5 +1,18 @@
 const usersCtrl = {};
-usersCtrl.getUsers = (req, res) => res.send('Users []')
-usersCtrl.createUser = (req, res) => res.send('User created')
-usersCtrl.deleteUser = (req, res) => res.send('User deleted')
+const User = require('../models/User');
+
+usersCtrl.getUsers = async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+};
+usersCtrl.createUser = async (req, res) => {
+    const {username} = req.body;
+    const newUser = new User({username});
+    await newUser.save();
+    res.json('User created');
+};
+usersCtrl.deleteUser = async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.json('User deleted');
+};
 module.exports = usersCtrl;
